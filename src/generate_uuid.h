55e8431a510c03b1105c3aa4ba85f15b994c4f00
gxrlxv <gorlov1207@gmail.com>
2022-04-30 00:40:43 +0300

    moved generate_uuid to a separate file

---
+++ src/generate_uuid.h
@@ -0,0 +1,10 @@
+//
+// Created by Sergey Gorlov on 30.04.2022.
+//
+
+#ifndef FLASHPROJECT_GENERATE_UUID_H
+#define FLASHPROJECT_GENERATE_UUID_H
+
+std::string generate_uuid();
+
+#endif //FLASHPROJECT_GENERATE_UUID_H
